4f5bfacef85e89c608ff0ff8ca40780b9fe4486c
Manal <manal_abd@hotmail.com>
2020-11-04 16:54:15 -0500

    restart button added

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ let hasFlippedCard = false;
 let lockedIn = false; //card isn't clicked
 let firstCard, secondCard;
 let score = 0;
+let startBtn = document.getElementById('startBtn')
+let restartBtn = document.getElementById('restart')
+
+
+// function start() {
+//   //if startBtn clicked
+// cards.forEach(card => card.removeEventListener('click', flipCard));
+// cards.style.display = "none"
+// startBtn.style.display = "none"
+// }
 
 function flipCard() { // this function flips the card when clicked
   if (lockedIn) return; //once clicked lockedIn is now true and will prevent any card flipping before the cards are hidden or matched.
@@ -17,7 +27,6 @@ function flipCard() { // this function flips the card when clicked
     secondCard = this; //flips the second card
     checkForMatch()
 }
-
 
 const checkForMatch = () => { //checks if cards match
    if (firstCard.dataset.framework ===  secondCard.dataset.framework) { //if first cards data.framework matches second cards data.framework then disable cards from being clicked
@@ -54,6 +63,18 @@ const resetBoard = () => { //using destructuring assignment syntax
   [firstCard, secondCard] = [null, null];
 
 }
+//function for restarting game. reset user score. flip back and reshuffle
+const restartGame = () => {
+  score = 0
+  cards.forEach(card => {
+    card.classList.remove('flip')
+    card.removeEventListener('click', flipCard)
+    card.addEventListener('click', flipCard)
+    let randomCard = Math.floor(Math.random() * 12);
+    card.style.order = randomCard;
+  })
+
+}
 
 (function shuffle() { //randomly places cards
   cards.forEach(card => {
@@ -63,3 +84,4 @@ const resetBoard = () => { //using destructuring assignment syntax
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+restartBtn.addEventListener('click', restartGame)
